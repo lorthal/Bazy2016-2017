909d96eb2c3cc8dfdfc5f70dbe9cfc84b75d639a
Lorthal <bkluchcinski@gmail.com>
2017-01-11 06:57:59 +0100

    dodano Procedure 2

--- procedures.sql
+++ procedures.sql
@@ -49,7 +49,7 @@ begin
 		if @ile > 0
 		begin
 			update Osoby
-			set nrZawodow = (select top 1 nrZawodow from Zawody where nazwaZawodow = @nazwaZawodow)
+			set nrZawodow = (select top 1 nrZawodow from Zawody where nazwaZawodow = @nazwaZawodow order by dataZawodow)
 			where nrOsoby = @nrOsoby
 
 			set @imie = (select imie from Osoby where nrOsoby = @nrOsoby)
@@ -66,4 +66,58 @@ end
 go
 
 -- Przyk³ad u¿ycia Procedury 1
-exec proc1 @nrOsoby = 1, @nazwaZawodow = 'Turniej siatkówki' 
+--exec proc1 @nrOsoby = 1, @nazwaZawodow = 'Turniej siatkówki'
+
+
+-- Procedura 2
+-- Dodanie nowego wynajêcia obiektu sportowego.
+
+if exists(select 1 from sys.objects where type = 'P' and name = 'proc2')
+	drop procedure proc2
+go
+create procedure proc2	@dataOd datetime = null,
+						@dataDo datetime = null,
+						@nrObiektu int = null,
+						@nrOsoby int = null,
+						@koszt decimal(6,2) = null
+as
+begin
+	if @dataOd is null or @dataDo is null or @nrObiektu is null or @nrOsoby is null or @koszt is null
+	begin
+		print 'Nie podano wszystkich danych.'
+	end
+	else
+	begin
+		declare @nazwaObiektu varchar(50)
+		set @nazwaObiektu = (select nazwaObiektu from ObiektSportowy where nrObiektu = @nrObiektu)
+
+		declare @imie varchar(50)
+		set @imie = (select imie from Osoby where nrOsoby = @nrOsoby)
+
+		declare @nazwisko varchar(50)
+		set @nazwisko = (select nazwisko from Osoby where nrOsoby = @nrOsoby)
+
+		if @nazwaObiektu is null
+		begin
+			print 'Obiekt o numerze: ' + cast(@nrObiektu as varchar) + ' nie istnieje'
+		end
+		else if @imie is null or @nazwisko is null
+		begin
+			print 'Nie ma osoby o numerze: ' + cast(@nrOsoby as varchar)
+		end 
+		else
+		begin
+			insert into WynajecieObiektu values(@dataOd, @dataDo, @nrObiektu, @nrOsoby, @koszt)
+			print 'Uda³o sie wynaj¹æ obiekt ' + @nazwaObiektu + ' osobie ' + @imie + ' ' + @nazwisko + ' w terminie od: ' + cast(@dataOd as varchar) + ' do: ' + cast(@dataDo as varchar) + ' za ' + cast(@koszt as varchar) 
+		end
+	end
+end
+go
+-- Przyk³ady u¿ycia Procedury 2
+exec proc2 '2017-04-12 09:00', '2017-04-12 10:00', 1 
+
+exec proc2 @dataOd = '2017-04-12 09:00', @dataDo ='2017-04-12 10:00',  @nrObiektu = 1, @nrOsoby = 10, @koszt = 10.00
+
+exec proc2 @dataOd = '2017-04-12 09:00', @dataDo ='2017-04-12 10:00',  @nrObiektu = 25, @nrOsoby = 10, @koszt = 10.00
+
+exec proc2 @dataOd = '2017-04-12 09:00', @dataDo ='2017-04-12 10:00',  @nrObiektu = 1, @nrOsoby = 49, @koszt = 10.00
