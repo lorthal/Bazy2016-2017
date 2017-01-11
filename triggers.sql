0fb6f1570743d6a0dfe694b640da93e35b960d6b
Lorthal <bkluchcinski@gmail.com>
2017-01-11 08:53:40 +0100

    dodano Wyzwalacz 2 i zmieniono pozwolenie na null wynajeciu obiektu

--- triggers.sql
+++ triggers.sql
@@ -1,7 +1,8 @@
 -- Wyzwalacz 1
 -- Usuwaj¹c Sprzêt usuwamy wszystkie wypozyczenia tego sprzêtu
 
-drop trigger trigger1
+if exists (select 1 from sys.triggers where name = 'trigger1')
+	drop trigger trigger1
 go
 create trigger trigger1
 on dbo.Sprzet
@@ -11,8 +12,36 @@ as
 	delete from dbo.Sprzet where nrSprzetu = (select nrSprzetu from deleted)
 go
 
-select * from WypozyczenieSprzetu
+-- Przyk³ad u¿ycia wyzwalacza 1
+--select * from WypozyczenieSprzetu
 
-delete dbo.Sprzet where nrSprzetu = 18
+--delete dbo.Sprzet where nrSprzetu = 18
 
-select * from WypozyczenieSprzetu
+--select * from WypozyczenieSprzetu
+
+
+-- Wyzwalacz 2
+-- Przy utworzeniu zawodów dodaje wynajêcie obiektu na 4 godziny
+
+if exists (select 1 from sys.triggers where name = 'trigger2')
+	drop trigger trigger2
+go
+create trigger trigger2
+on dbo.Zawody
+instead of insert
+as
+
+	insert into WynajecieObiektu values((select inserted.dataZawodow from inserted), 
+		DATEADD(hour, 4, (select inserted.dataZawodow from inserted)),
+		(select inserted.nrObiektu from inserted), null, 0.00 )
+
+		insert into dbo.Zawody (nazwaZawodow, dataZawodow, nrObiektu)
+		select nazwaZawodow, dataZawodow, nrObiektu from inserted
+go
+
+-- Przyk³ad u¿ycia wyzwalacza 2
+--select * from WynajecieObiektu
+
+--insert into Zawody values('Turniej koszykówki', '2017-06-01 10:00', 2)
+
+--select * from WynajecieObiektu
